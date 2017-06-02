7bf57375405e20d5470bd5ece23f686a356dac39
s880367 <s880367@gmail.com>
2017-06-02 13:25:14 +0800

    import tm_modern driver

--- src/tm_otg.cpp
+++ src/tm_otg.cpp
@@ -2,9 +2,9 @@
  *          GNU LESSER GENERAL PUBLIC LICENSE
  *              Version 3, 29 June 2007
  * 
- *Copyright (c) 2017, Howard Chen, ISCI lab, NCTU.
+ *Copyright (c) 2017, ISCI / National Chiao Tung University (NCTU)
  * 
- *Author :  Howard Chen
+ *Author :  Howard Chen, howardchen.ece04g@g2.nctu.edu.tw
  *
  *This version of the GNU Lesser General Public License incorporates
  *the terms and conditions of version 3 of the GNU General Public License.
@@ -50,6 +50,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <chrono>
+#include <thread>
 
 #include <ReflexxesAPI.h>
 #include <RMLPositionFlags.h>
@@ -62,6 +64,8 @@
 
 #define CYCLE_TIME_IN_SECONDS                   0.025
 #define NUMBER_OF_DOFS                          1
+#define DEG2RAD 0.01745329252
+#define RAD2DEG 57.29577951
 
 int main()
 {
@@ -92,7 +96,7 @@ int main()
     OP = new RMLPositionOutputParameters(NUMBER_OF_DOFS);
 
 
-    IP->CurrentPositionVector->VecData      [0] =    1.0487      ;
+    IP->CurrentPositionVector->VecData      [0] =    0.0      ;
     //IP->CurrentPositionVector->VecData      [1] =      0.0      ;
     //IP->CurrentPositionVector->VecData      [2] =     50.0      ;
 
@@ -100,15 +104,15 @@ int main()
     //IP->CurrentVelocityVector->VecData      [1] =   -220.0      ;
     //IP->CurrentVelocityVector->VecData      [2] =    -50.0      ;
 
-    IP->CurrentAccelerationVector->VecData  [0] =   -0.5184      ;
+    IP->CurrentAccelerationVector->VecData  [0] =   0.0      ;
     //IP->CurrentAccelerationVector->VecData  [1] =    250.0      ;
     //IP->CurrentAccelerationVector->VecData  [2] =    -50.0      ;
 
-    IP->MaxVelocityVector->VecData          [0] =    0.317      ;
+    IP->MaxVelocityVector->VecData          [0] =    0.3247      ;
     //IP->MaxVelocityVector->VecData          [1] =    100.0      ;
     //IP->MaxVelocityVector->VecData          [2] =    300.0      ;
 
-    IP->MaxAccelerationVector->VecData      [0] =     3.18;
+    IP->MaxAccelerationVector->VecData      [0] =     0.6494;
     //IP->MaxAccelerationVector->VecData      [1] =    200.0      ;
     //IP->MaxAccelerationVector->VecData      [2] =    100.0      ;
 
@@ -116,11 +120,11 @@ int main()
     //IP->MaxJerkVector->VecData              [1] =    300.0      ;
     //IP->MaxJerkVector->VecData              [2] =    200.0      ;
 
-    IP->TargetPositionVector->VecData       [0] =   1.838      ;
+    IP->TargetPositionVector->VecData       [0] =   1.57      ;
     //IP->TargetPositionVector->VecData       [1] =   -200.0      ;
     //IP->TargetPositionVector->VecData       [2] =   -350.0      ;
 
-    IP->TargetVelocityVector->VecData       [0] =    0.098       ;
+    IP->TargetVelocityVector->VecData       [0] =    0.0       ;
     //IP->TargetVelocityVector->VecData       [1] =   -50.0       ;
     //IP->TargetVelocityVector->VecData       [2] =  -200.0       ;
 
@@ -131,32 +135,23 @@ int main()
     // ********************************************************************
     // Specifying the minimum synchronization time
 
-    IP->MinimumSynchronizationTime              =   3.0         ;
+    IP->MinimumSynchronizationTime              =   10.0         ;
 
     // ********************************************************************
     // Checking the input parameters (optional)
 
     if (IP->CheckForValidity())
-    {
         printf("Input values are valid!\n");
-    }
     else
-    {
         printf("Input values are INVALID!\n");
-    }
 
     // ********************************************************************
     // Starting the control loop
 
+    int cycle_iteration = 0;
+
     while (ResultValue != ReflexxesAPI::RML_FINAL_STATE_REACHED)
     {
-
-        // ****************************************************************
-        // Wait for the next timer tick
-        // (not implemented in this example in order to keep it simple)
-        // ****************************************************************
-
-        // Calling the Reflexxes OTG algorithm
         ResultValue =  RML->RMLPosition(*IP, OP, Flags );
 
         if (ResultValue < 0)
@@ -165,134 +160,21 @@ int main()
             break;
         }
 
-        // ****************************************************************
-        // The following part completely describes all output values
-        // of the Reflexxes Type II Online Trajectory Generation
-        // algorithm.
+        printf("%10.4lf ", OP->NewPositionVector->VecData[j]*RAD2DEG);
+        printf("%10.4lf ", OP->NewVelocityVector->VecData[j]);
+        printf("%10.4lf ", OP->NewAccelerationVector->VecData[j]);
+        printf("[%10.3f ] ", ++cycle_iteration*0.025 );
+        printf("\n");
 
-        if (!FirstCycleCompleted)
-        {
-            //FirstCycleCompleted =   true;
+        *IP->CurrentPositionVector =  *OP->NewPositionVector;
+        *IP->CurrentVelocityVector =  *OP->NewVelocityVector;
+        *IP->CurrentAccelerationVector =  *OP->NewAccelerationVector;
+        std::this_thread::sleep_for(std::chrono::milliseconds(25));
 
-            /*printf("-------------------------------------------------------\n");
-            printf("General information:\n\n");
-
-            printf("The execution time of the current trajectory is %.3lf seconds.\n", OP->GetSynchronizationTime());
-
-            if (OP->IsTrajectoryPhaseSynchronized())
-            {
-                printf("The current trajectory is phase-synchronized.\n");
-            }
-            else
-            {
-                printf("The current trajectory is time-synchronized.\n");
-            }
-            if (OP->WasACompleteComputationPerformedDuringTheLastCycle())
-            {
-                printf("The trajectory was computed during the last computation cycle.\n");
-            }
-            else
-            {
-                printf("The input values did not change, and a new computation of the trajectory parameters was not required.\n");
-            }
-
-            printf("-------------------------------------------------------\n");
-            printf("New state of motion:\n\n");
-*/
-
-            printf("%10.3lf ", OP->NewPositionVector->VecData[j]);
-            printf("%10.3lf ", OP->NewVelocityVector->VecData[j]);
-            printf("%10.3lf ", OP->NewAccelerationVector->VecData[j]);
-            printf("\n");
-/*
-            printf("New position/pose vector                  : ");
-            for ( j = 0; j < NUMBER_OF_DOFS; j++)
-            {
-                printf("%10.3lf ", OP->NewPositionVector->VecData[j]);
-            }
-            printf("\n");
-            printf("New velocity vector                       : ");
-            for ( j = 0; j < NUMBER_OF_DOFS; j++)
-            {
-                printf("%10.3lf ", OP->NewVelocityVector->VecData[j]);
-            }
-            printf("\n");
-            printf("New acceleration vector                   : ");
-            for ( j = 0; j < NUMBER_OF_DOFS; j++)
-            {
-                printf("%10.3lf ", OP->NewAccelerationVector->VecData[j]);
-            }
-            printf("\n");
-            printf("-------------------------------------------------------\n");
-            printf("Extremes of the current trajectory:\n");
-
-            for ( i = 0; i < NUMBER_OF_DOFS; i++)
-            {
-                printf("\n");
-                printf("Degree of freedom                         : %d\n", i);
-                printf("Minimum position                          : %10.3lf\n", OP->MinPosExtremaPositionVectorOnly->VecData[i]);
-                printf("Time, at which the minimum will be reached: %10.3lf\n", OP->MinExtremaTimesVector->VecData[i]);
-                printf("Position/pose vector at this time         : ");
-                for ( j = 0; j < NUMBER_OF_DOFS; j++)
-                {
-                    printf("%10.3lf ", OP->MinPosExtremaPositionVectorArray[i]->VecData[j]);
-                }
-                printf("\n");
-                printf("Velocity vector at this time              : ");
-                for ( j = 0; j < NUMBER_OF_DOFS; j++)
-                {
-                    printf("%10.3lf ", OP->MinPosExtremaVelocityVectorArray[i]->VecData[j]);
-                }
-                printf("\n");
-                printf("Acceleration vector at this time          : ");
-                for ( j = 0; j < NUMBER_OF_DOFS; j++)
-                {
-                    printf("%10.3lf ", OP->MinPosExtremaAccelerationVectorArray[i]->VecData[j]);
-                }
-                printf("\n");
-                printf("Maximum position                          : %10.3lf\n", OP->MaxPosExtremaPositionVectorOnly->VecData[i]);
-                printf("Time, at which the maximum will be reached: %10.3lf\n", OP->MaxExtremaTimesVector->VecData[i]);
-                printf("Position/pose vector at this time         : ");
-                for ( j = 0; j < NUMBER_OF_DOFS; j++)
-                {
-                    printf("%10.3lf ", OP->MaxPosExtremaPositionVectorArray[i]->VecData[j]);
-                }
-                printf("\n");
-                printf("Velocity vector at this time              : ");
-                for ( j = 0; j < NUMBER_OF_DOFS; j++)
-                {
-                    printf("%10.3lf ", OP->MaxPosExtremaVelocityVectorArray[i]->VecData[j]);
-                }
-                printf("\n");
-                printf("Acceleration vector at this time          : ");
-                for ( j = 0; j < NUMBER_OF_DOFS; j++)
-                {
-                    printf("%10.3lf ", OP->MaxPosExtremaAccelerationVectorArray[i]->VecData[j]);
-                }
-                printf("\n");
-            }
-            printf("-------------------------------------------------------\n");
-        */
-        }
-
-        // ****************************************************************
-
-        // ****************************************************************
-        // Feed the output values of the current control cycle back to
-        // input values of the next control cycle
-
-        *IP->CurrentPositionVector      =   *OP->NewPositionVector      ;
-        *IP->CurrentVelocityVector      =   *OP->NewVelocityVector      ;
-        *IP->CurrentAccelerationVector  =   *OP->NewAccelerationVector  ;
     }
 
-    // ********************************************************************
-    // Deleting the objects of the Reflexxes Motion Library end terminating
-    // the process
-
-    delete  RML         ;
-    delete  IP          ;
-    delete  OP          ;
-
-    exit(EXIT_SUCCESS)  ;
+    delete  RML;
+    delete  IP;
+    delete  OP;
+    exit(EXIT_SUCCESS);
 }
