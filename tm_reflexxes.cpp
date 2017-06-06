44e2b81f7e459befd3849738c04ccbc07e2e53d9
s880367 <s880367@gmail.com>
2017-06-06 22:37:40 +0800

    update branch

--- tm_reflexxes.cpp
+++ tm_reflexxes.cpp
@@ -641,10 +641,6 @@ int main(int argc, char **argv)
                     break;
                 }
 
-//                time_s = TmRobot.interface->stateRT->getQAct(CurrentPosition);
-//                time_s = TmRobot.interface->stateRT->getQdAct(CurrentVelocity);
-//                printf("NOW : %lf %10.4lf %10.4lf  ",time_s, CurrentPosition[4], CurrentVelocity[4]);
-
                 vec = { OP->NewVelocityVector->VecData[0],
                         OP->NewVelocityVector->VecData[1],
                         OP->NewVelocityVector->VecData[2],
@@ -653,9 +649,6 @@ int main(int argc, char **argv)
                         OP->NewVelocityVector->VecData[5]};
 
                 TmRobot.setMoveJointSpeedabs(vec, blend);
-
-//                printf(" | CMD : %10.4lf ", OP->NewPositionVector->VecData[0]);
-//                printf("%10.4lf  \n", OP->NewVelocityVector->VecData[0]);
 
                 time_s = TmRobot.interface->stateRT->getTime();
                 printf("[ %lf ]  ",time_s );
